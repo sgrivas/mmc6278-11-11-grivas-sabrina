9e4c36289e330e8d9098c112b2c38665ab4befda
Sabrina Grivas <sgrivas1@gmail.com>
2022-07-27 08:00:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const getPoemBtn = document.getElementById('get-poem')
+const poemEl = document.getElementById('poem')
+const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json'
+
+const getJSON = url => fetch(url).then(res => res.json())
+
+const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
+
+const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+
+// complete this function
+const makePoemHTML = ([
+  {
+    title,
+    author,
+    lines
+  }
+]) => {
+  const titleEl = makeTag('h2')(title)
+  console.log(titleEl);
+}
+
+// attach a click event to #get-poem
+getPoemBtn.onclick = async function() {
+  // renders the HTML string returned by makePoemHTML to #poem
+  poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
+}
