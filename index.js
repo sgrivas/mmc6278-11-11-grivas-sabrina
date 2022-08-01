fc8b82d92c9aab982bf1b9bb1f51435289a4ed07
Sabrina Grivas <sgrivas1@gmail.com>
2022-07-31 21:56:47 -0400

    append elements to html

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const getJSON = url => fetch(url).then(res => res.json())
 const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
 
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+const makeBr = tag => str => `${str}<${tag}>`
 
 const makeEm= makeTag('em')
 const makeH3= makeTag('h3')
@@ -20,18 +21,23 @@ const makePoemHTML = ([
   }
 ]) => {
   const authorFn = pipe(makeEm, makeH3)
-  poemEl.innerHTML=`
+  const linesBr=lines.map(line => {
+    return makeBr('br')(line)
+  });
+  const joinLines=linesBr.join(' ')
+  const splitLines=joinLines.split(' <br>')
+  const stanza = splitLines.map(line=>{
+    return makeTag('p')(line)
+  })
+  return `
   ${makeTag('h2')(title)}
   ${authorFn(`by ${author}`)}
+  ${stanza}
   `
-  console.log(lines);
-  const stanza = lines.map(line => {
-    
-  })
 }
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
   // renders the HTML string returned by makePoemHTML to #poem
-  poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
+  poemEl.innerHTML= makePoemHTML(await getJSON(poemURL))
 }
